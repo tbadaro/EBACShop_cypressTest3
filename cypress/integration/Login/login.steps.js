cd031b5b310b9deaf16b556ec82133368c436242
tbadaro <t.badaro@gmail.com>
2022-03-14 15:55:30 -0300

    First commit

---
+++ cypress/integration/Login/login.steps.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+import {Given, When, Then} from "cypress-cucumber-preprocessor/steps" 
+
+const { dashboardPage } = require('../../support/pages')
+
+Given('I visit EBACStore', () =>{
+    cy.visit('/')
+})
+
+When("When I log in with user {string} and pass {string}", (user, pass) =>{    
+    cy.login(user, pass)   
+})
+
+Then('Then the admin dashboard page should be visible', () =>{
+    dashboardPage.siteName.should('be.visible');
+})
+
